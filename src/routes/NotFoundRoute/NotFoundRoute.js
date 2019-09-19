6db7ba603058ba395618e6707f61f45fd5428707
Colleen <colleenhiggins87@gmail.com>
2019-09-19 14:20:54 -0700

    added not found mg

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import pumpkin from '../../Media/infant-pumpkin-costume.jpg'
+import '../NotFoundRoute/NotFound.css'
 
 class NotFoundRoute extends Component {
   render() {
     return (
-      <section>
+      <section className="not-found">
         <h2>404 - Page not found</h2>
+        <div className = 'not-found-image'>
+        <img className = "not-found-img" src ={pumpkin} alt="child in pumpkin costume"/>
+        </div>
         <p>Try going back to your previous page.</p>
       </section>
     );
