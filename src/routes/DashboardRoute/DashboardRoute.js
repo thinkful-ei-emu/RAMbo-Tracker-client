d7e5f61f9d89846bb877c51467d6f5c747576b5e
David Queen <dsqjr@msn.com>
2019-09-19 15:06:30 -0400

    modals

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import API from '../../services/Api-service';
+import Modal from 'react-modal';
+//components
+import Symptoms from '../../components/Symptom/Symptom';
+import Meal from '../MealRoute/MealRoute';
+
+//to be removed for final product
 import helper from '../../services/helper.services';
 
 export default class DashBoard extends React.Component{
   state = {
+    addMealModal : false,
+    addSymptomsModal :false,
     
       user :{
           username:'testUser',
           display_name:'Jim Carey'},
-
       events: 
-        [
-          {
+        [{
+            time: 2134234,
             type: '', 
             items: [
               {
@@ -23,7 +30,7 @@ export default class DashBoard extends React.Component{
                 ingredients: []
               }
             ],
-            time: 2134234
+            
           }, 
           {
             type: '', 
@@ -41,11 +48,28 @@ export default class DashBoard extends React.Component{
         user: helper.getUserEvents()
       })
   }
+
+  closeModal = (modal)=>
+  {
+    this.setState({[modal]:false});
+  }
+  openModal = (e,modal)=>{
+    e.preventDefault();
+    this.setState({[modal]:true});
+  }
   render(){
     return (
     <div>
+      {/*add meal modal*/}
+      <Modal isOpen={this.state.addMealModal} onRequestClose={(e)=>this.closeModal('addMealModal')}>
+        <Meal/>
+      </Modal>
+      <Modal isOpen={this.state.addSymptomsModal} onRequestClose={()=>this.closeModal('addSymptomsModal')}>
+      <Symptoms/>
+      </Modal>
       Welcome back <strong>{this.state.user.display_name}</strong>
-      <button>+</button>
+      <button onClick={(e)=>this.openModal(e,'addMealModal')}>New Meal</button>
+      <button onClick={(e)=>this.openModal(e,'addSymptomsModal')}>New symptoms</button>
     </div>)
   }
 }
