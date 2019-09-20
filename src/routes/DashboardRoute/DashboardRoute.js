b6096e68c0d5f5f29efaad73572eb2eb68f9ad09
Colleen <colleenhiggins87@gmail.com>
2019-09-20 08:49:28 -0700

    styling

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,84 +1,108 @@
-import React from 'react';
-import API from '../../services/Api-service';
-import Modal from 'react-modal';
+import React from "react";
+import API from "../../services/Api-service";
+import Modal from "react-modal";
 //components
-import Symptoms from '../../components/Symptom/Symptom';
-import Meal from '../MealRoute/MealRoute';
+import Symptoms from "../../components/Symptom/Symptom";
+import Meal from "../MealRoute/MealRoute";
 //css
-import './Dashboard.css';
+import "./Dashboard.css";
 //to be removed for final product
-import helper from '../../services/helper.services';
+import helper from "../../services/helper.services";
 
-export default class DashBoard extends React.Component{
+export default class DashBoard extends React.Component {
   state = {
-    addMealModal : false,
-    addSymptomsModal :false,
-    
-      user :{
-          username:'',
-          display_name:''},
-      events: 
-        [{
-            name:'',
-            time: 2134234,
-            type: '', 
-            items: [
-              {
-                name: '',
-                ingredients: []
-              },
-              {
-                name: '',
-                ingredients: []
-              }
-            ],
-          }, 
+    addMealModal: false,
+    addSymptomsModal: false,
+
+    user: {
+      username: "",
+      display_name: ""
+    },
+    events: [
+      {
+        name: "",
+        time: 2134234,
+        type: "",
+        items: [
           {
-            type: '', 
-            name: '', 
-            severity: 0, 
-            time: null
+            name: "",
+            ingredients: []
+          },
+          {
+            name: "",
+            ingredients: []
           }
-        ],
-        symptoms: []
-    }
-  componentDidMount(){
-   /*  API.doFetch('events')
+        ]
+      },
+      {
+        type: "",
+        name: "",
+        severity: 0,
+        time: null
+      }
+    ],
+    symptoms: []
+  };
+  componentDidMount() {
+    /*  API.doFetch('events')
       .then(res=>) */
-      let test = helper.getUserEvents()
-      this.setState({
-        user: {username : test.username,display_name: test.display_name},
-        events: test.events
-      })
+    let test = helper.getUserEvents();
+    this.setState({
+      user: { username: test.username, display_name: test.display_name },
+      events: test.events
+    });
   }
 
-  closeModal = (modal)=>
-  {
-    this.setState({[modal]:false});
-  }
-  openModal = (e,modal)=>{
+  closeModal = modal => {
+    this.setState({ [modal]: false });
+  };
+  openModal = (e, modal) => {
     e.preventDefault();
-    this.setState({[modal]:true});
-  }
-  render(){
-    let events = this.state.events.map((e,index)=>{
-      return (<li key={index} className={e.type === 'meal'? 'meal': 'symptom'}>{e.name}@ {e.time} | {e.severity}</li>)
-    })
+    this.setState({ [modal]: true });
+  };
+  render() {
+    let events = this.state.events.map((e, index) => {
+      return (
+        <div className="events-list">
+        <li key={index} className={e.type === "meal" ? "meal" : "symptom"}>
+          {e.name} at {e.time} | severity: {e.severity}
+        </li>
+        </div>
+      );
+    });
     return (
-    <div>
-      {/*add meal modal*/}
-      <Modal isOpen={this.state.addMealModal} onRequestClose={(e)=>this.closeModal('addMealModal')}>
-        <Meal closeModal={this.closeModal}/>
-      </Modal>
-      <Modal isOpen={this.state.addSymptomsModal} onRequestClose={()=>this.closeModal('addSymptomsModal')}>
-      <Symptoms closeModal={this.closeModal}/>
-      </Modal>
-      Welcome back <strong>{this.state.user.display_name}</strong>
-      <div className="events">
-        {events}
+      <div className="user-dashboard">
+        {/*add meal modal*/}
+        <Modal
+          isOpen={this.state.addMealModal}
+          onRequestClose={e => this.closeModal("addMealModal")}
+        >
+          <Meal closeModal={this.closeModal} />
+        </Modal>
+        <Modal
+          isOpen={this.state.addSymptomsModal}
+          onRequestClose={() => this.closeModal("addSymptomsModal")}
+        >
+          <Symptoms closeModal={this.closeModal} />
+        </Modal>
+        <h3 id="user-welcome">Welcome back, {this.state.user.display_name}</h3>
+        <div id="dash-button-container">
+        <button
+          className="user-button"
+          onClick={e => this.openModal(e, "addMealModal")}
+        >
+          Add New Meal
+        </button>
+        <button
+          className="user-button"
+          onClick={e => this.openModal(e, "addSymptomsModal")}
+        >
+          Add New Symptoms
+        </button>
+        </div>
+        <div className="events">{events}</div>
+       
       </div>
-      <button className="user-button" onClick={(e)=>this.openModal(e,'addMealModal')}>New Meal</button>
-      <button className="user-button" onClick={(e)=>this.openModal(e,'addSymptomsModal')}>New symptoms</button>
-    </div>)
+    );
   }
 }
