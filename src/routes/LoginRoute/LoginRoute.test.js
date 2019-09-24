97c27bb5c2fb20b0b8ca570db6009daf4a872433
Colleen <colleenhiggins87@gmail.com>
2019-09-24 11:43:22 -0700

    testing snapshots

--- src/routes/LoginRoute/LoginRoute.test.js
+++ src/routes/LoginRoute/LoginRoute.test.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import LoginRoute from './LoginRoute';
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer'
-
+import LoginForm from '../../components/LoginForm/LoginForm'
 it('renders without crashing', () => {
+  
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
@@ -15,12 +16,19 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 it("renders the UI as expected", () => {
+  const div = document.createElement('div')
+  const wrapper = ReactDOM.render(<LoginForm />, div);
+    const { inputRef } = wrapper.instance();
 
-  const tree = renderer
-    .create(<BrowserRouter>
-      <LoginRoute />
-    </BrowserRouter>
-    )
-    .toJSON();
+    jest.spyOn(inputRef.current, "focus");
+
+    wrapper.instance().componentDidMount();
+    expect(inputRef.current.focus).toHaveBeenCalledTimes(1);
+ 
+ const tree = renderer.create(<BrowserRouter>
+  <LoginRoute input={wrapper}/>
+</BrowserRouter>)
+ 
+   
   expect(tree).toMatchSnapshot();
 });
