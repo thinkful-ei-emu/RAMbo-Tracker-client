fa66172460913deb5e616a3cd5a6ba4b42fb89b5
Colleen <colleenhiggins87@gmail.com>
2019-09-24 09:21:28 -0700

    app.js test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {BrowserRouter} from 'react-router-dom'
 //import ReactModal from "react-modal";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   
-  
 
-    ReactDOM.render(<App />, div);
+
+    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 
 });
