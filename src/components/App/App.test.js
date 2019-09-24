da074ad904637d071b1ef2bae36e2470e13b8672
Colleen <colleenhiggins87@gmail.com>
2019-09-24 09:11:05 -0700

    modal test bug

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//import ReactModal from "react-modal";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  
+  
+
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+
 });
