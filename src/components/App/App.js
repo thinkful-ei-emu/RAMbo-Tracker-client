4fc24c6c74ba7e4b995ffe71f73644151d784d95
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-25 14:58:24 -0500

    also deleted the Router import in App.js just because

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import TokenService from '../../services/token-service';
