5e42d67ab92fdac4938b5daceb7e5335cdb7ccd1
Colleen <colleenhiggins87@gmail.com>
2019-10-01 07:46:16 -0700

    changed main page images

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 import './RegistrationRoute.css'
-import lemon from '../../Media/Lemons.jpg'
-import carrot from '../../Media/carrot.jpg'
-import grape from '../../Media/grapes.jpg'
-import lettuce from '../../Media/lettuce.jpg'
+import burger from '../../Media/burger.png'
+import pizza from '../../Media/pizza.png'
+import popcorn from '../../Media/popcorn.png'
+import eggroll from '../../Media/eggroll.png'
 
 class RegistrationRoute extends React.Component {
   handleRegistrationSuccess = () => {
@@ -21,12 +21,12 @@ class RegistrationRoute extends React.Component {
         <div className='reg-photo-container'>
         <img
           className="reg-photo"
-          src={lemon}
-          alt="A lemon"
+          src={popcorn}
+          alt="A painting of a burger"
         /></div>
         <img
           className="reg-photo-2"
-          src={carrot}
+          src={burger}
           alt="Carrot"
         />
         <div className="mission">
@@ -48,13 +48,13 @@ class RegistrationRoute extends React.Component {
           </div>
           <img
           className="reg-photo-2"
-          src={grape}
-          alt="bunch of grapes"
+          src={pizza}
+          alt="half eaten pizza"
         />
              <img
           className="reg-photo-2"
-          src={lettuce}
-          alt="lettuce"
+           src={eggroll}
+           alt="eggroll"
         />
         <div className="mission">
           <p>
