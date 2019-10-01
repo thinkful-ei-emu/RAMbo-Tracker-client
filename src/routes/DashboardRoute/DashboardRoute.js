01c1609d82958fabef504b3e238e20613a883491
Colleen <colleenhiggins87@gmail.com>
2019-10-01 14:15:53 -0700

    added border/color to id meal and symptom in log

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -177,7 +177,7 @@ export default class DashBoard extends React.Component {
     let events = this.state.events.map((e, index) => {
       if (e.type === "meal") {
         return (
-          <div key={index} className="dash-event-container">
+          <div key={index} className="dash-event-container-meal">
             <li className={"meal"}>
               {e.name} at {this.formatDate(e.time)}
               <div className='meal-toggle-cont'>
@@ -227,7 +227,7 @@ export default class DashBoard extends React.Component {
         );
       } else {
         return (
-          <div key={index} className="dash-event-container">
+          <div key={index} className="dash-event-container-symptom">
             <li className="symptom">
               {e.name} at {this.formatDate(e.time)}{" "}
               {e.type === "symptom" ? `Severity: ${e.severity}` : ""}{' '}
