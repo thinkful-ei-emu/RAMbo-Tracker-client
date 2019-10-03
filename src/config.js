8a10811441958624a21bdfda3df16ab530f77643
tarajeanne <t.jeanne.patel@gmail.com>
2019-10-03 13:48:41 -0500

    made some changes to food results

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://rambo-tracker.herokuapp.com/api',
+  //API_ENDPOINT: 'https://rambo-tracker.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'rambo-tracker-auth-token',
 }
