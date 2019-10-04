ab3f2e17de55e743353037c271c8bdd09a337484
Colleen <colleenhiggins87@gmail.com>
2019-10-04 11:24:29 -0700

    config pointing to server

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 export default {
-<<<<<<< HEAD
-   /*API_ENDPOINT: 'https://rambo-tracker.herokuapp.com/api',*/
-   API_ENDPOINT: 'http://localhost:8000/api', 
-=======
-  // API_ENDPOINT: 'https://rambo-tracker.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
->>>>>>> cc553826d33e0057975470f233734ec95e279d41
-  TOKEN_KEY: 'rambo-tracker-auth-token',
+  API_ENDPOINT: 'https://rambo-tracker.herokuapp.com/api',
+/*   API_ENDPOINT: 'http://localhost:8000/api',
+ */  TOKEN_KEY: 'rambo-tracker-auth-token',
 }
